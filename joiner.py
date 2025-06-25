da1edc3aebe79926abbe1102be22f89d86c2aa74
fangrong <darkbluesky@outlook.com>
2025-06-25 14:35:43 +0800

    Added eng cheats

--- joiner.py
+++ joiner.py
@@ -5,65 +5,47 @@ import json
 import bz2
 
 
-def compile_db(db_console_name):
+def compile_db(db_language_code):
     db = {}
-    print(f"Compiling cheat code db for {db_console_name}...")
+    print(f"Compiling cheat code db for {db_language_code}...")
 
-    if '3ds' in db_console_name:
-        for cheat in os.listdir('./3ds'):
-            with open(os.path.join('./3ds', cheat), 'r', encoding="UTF-8") as file:
-                titleid = cheat[:cheat.rfind('.')]
-                lines = [line.strip() for line in file]
-                lines = list(filter(None, lines))
+    dirname = ''
+    if 'chs' in db_language_code:
+        dirname = './3ds'
+    elif 'eng' in db_language_code:
+        dirname = './eng'
 
-                db[titleid] = {}
-                selectedCheat = lines[0]
-                for line in lines:
-                    if line.startswith('{') and line.endswith('}'):
-                        pass
-                    elif line.startswith('[') and line.endswith(']'):
-                        selectedCheat = line[1:-1]
-                        db[titleid][selectedCheat] = []
-                    else:
-                        db[titleid][selectedCheat].append(line)
-    elif 'switch' in db_console_name:
-        for root, _, files in os.walk('./switch'):
-            root = root.replace('\\', '/')
-            titleid = root[root.rfind('/') + 1:]
-
-            if "switch" in titleid:
-                continue
+    for cheat in os.listdir(dirname):
+        with open(os.path.join(dirname, cheat), 'r', encoding="UTF-8") as file:
+            titleid = cheat[:cheat.rfind('.')]
+            lines = [line.strip() for line in file]
+            lines = list(filter(None, lines))
 
             db[titleid] = {}
-            for file in files:
-                buildid = file[:file.find('.')]
-                db[titleid][buildid] = {}
-                with open(os.path.join(root, file), 'r', encoding="UTF-8") as f:
-                    lines = [line.strip() for line in f]
-                    lines = list(filter(None, lines))
-                    selectedCheat = lines[0]
-                    for line in lines:
-                        if line.startswith('{') and line.endswith('}'):
-                            pass
-                        elif line.startswith('[') and line.endswith(']'):
-                            selectedCheat = line[1:-1]
-                            db[titleid][buildid][selectedCheat] = []
-                        else:
-                            db[titleid][buildid][selectedCheat].append(line)
+            selectedCheat = lines[0]
+            for line in lines:
+                if line.startswith('{') and line.endswith('}'):
+                    pass
+                elif line.startswith('[') and line.endswith(']'):
+                    selectedCheat = line[1:-1]
+                    db[titleid][selectedCheat] = []
+                else:
+                    db[titleid][selectedCheat].append(line)
+
     compressed = bz2.compress(str.encode(json.dumps(db)))
-    with open(os.path.join('build', db_console_name + '.json'), 'w') as f:
+    with open(os.path.join('build', '3ds_' + db_language_code + '.json'), 'w') as f:
         f.write(json.dumps(db))
-    with open(os.path.join('build', db_console_name + '.json.bz2'), 'wb') as f:
+    with open(os.path.join('build', '3ds_' + db_language_code + '.json.bz2'), 'wb') as f:
         f.write(compressed)
 
 
 if __name__ == '__main__':
-    args = list(i for i in sys.argv[1:] if i in ("3ds", "switch"))
+    args = list(i for i in sys.argv[1:] if i in ("chs", "eng"))
     if len(sys.argv) == 1:
-        args = ["3ds", "switch"]
+        args = ["chs", "eng"]
     if not args:
-        print("Proper argument options are '3ds' or 'switch', or nothing to compile both.")
+        print("Proper argument options are 'chs' or 'eng', or nothing to compile both.")
         exit(0)
     for arg in args:
         compile_db(arg)
-        print(f"Wrote compiled cheat code DB to 'build/{arg}.json'.")
+        print(f"Wrote compiled cheat code DB to 'build/3ds_{arg}.json'.")
