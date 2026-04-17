5b8bee69185d3411fde764c19ea2e35a290deeb6
fangrong <darkbluesky@outlook.com>
2026-04-17 16:06:34 +0800

    Merge cheats from `ibnux/switch-cheat`

--- joiner.py
+++ joiner.py
@@ -9,24 +9,7 @@ def compile_db(db_console_name):
     db = {}
     print(f"Compiling cheat code db for {db_console_name}...")
 
-    if '3ds' in db_console_name:
-        for cheat in os.listdir('./3ds'):
-            with open(os.path.join('./3ds', cheat), 'r', encoding="UTF-8") as file:
-                titleid = cheat[:cheat.rfind('.')]
-                lines = [line.strip() for line in file]
-                lines = list(filter(None, lines))
-
-                db[titleid] = {}
-                selectedCheat = lines[0]
-                for line in lines:
-                    if line.startswith('{') and line.endswith('}'):
-                        pass
-                    elif line.startswith('[') and line.endswith(']'):
-                        selectedCheat = line[1:-1]
-                        db[titleid][selectedCheat] = []
-                    else:
-                        db[titleid][selectedCheat].append(line)
-    elif 'switch' in db_console_name:
+    if 'switch' in db_console_name:
         for root, _, files in os.walk('./switch'):
             root = root.replace('\\', '/')
             titleid = root[root.rfind('/') + 1:]
@@ -52,17 +35,17 @@ def compile_db(db_console_name):
                             db[titleid][buildid][selectedCheat].append(line)
     compressed = bz2.compress(str.encode(json.dumps(db)))
     with open(os.path.join('build', db_console_name + '.json'), 'w') as f:
-        f.write(json.dumps(db))
+        f.write(json.dumps(db, indent=2))
     with open(os.path.join('build', db_console_name + '.json.bz2'), 'wb') as f:
         f.write(compressed)
 
 
 if __name__ == '__main__':
-    args = list(i for i in sys.argv[1:] if i in ("3ds", "switch"))
+    args = list(i for i in sys.argv[1:] if i in ("switch"))
     if len(sys.argv) == 1:
-        args = ["3ds", "switch"]
+        args = ["switch"]
     if not args:
-        input("Proper argument options are '3ds' or 'switch', or nothing to compile both.")
+        input("Proper argument options is 'switch', or nothing.")
         exit(0)
     for arg in args:
         compile_db(arg)
